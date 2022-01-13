454abbae4b31d495b87753bdf7f4942ff24b5861
maryferrer <mary.ferrer6@gmail.com>
2022-01-13 11:44:30 -0800

    handleDelete function to delete blogs from Home page

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
+const BlogList = ({ blogs, title, handleDelete }) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
 
     return ( 
         <div className="blog-list">
@@ -9,6 +9,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
