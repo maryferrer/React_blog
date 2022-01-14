10d506f95981b5a0e32d5003d872cd563c193b78
maryferrer <mary.ferrer6@gmail.com>
2022-01-13 16:40:28 -0800

    add create new blog form to Create.js component

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,48 @@
+import { useState } from "react";
+
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author }
+}
+
     return ( 
         <div className="create">
             <h2>Add A New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input type="text" 
+                required 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <label>Blog body:</label>
+                <textarea required
+                value={body}
+                onChange={(e) => setBody(e.target.value)}>
+                </textarea>
+
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+
+                <button>Add Blog</button>
+                <p>{ title }</p> 
+                <p>{ body }</p> 
+                <p>{ author }</p> 
+
+            </form>
         </div>
      );
 }
